c6c08f404b28ff08ded10168b103b067a8cb29cd
ijortin <ian.j.ortin@gmail.com>
2023-04-24 01:05:44 -0500

    Swapped from C++ to Blueprints
    Both C++ and Blueprints can be used, but the guide I'm using better follows with Blueprints template.

--- README.md
+++
@@ -1,7 +0,0 @@
-<<<<<<< Updated upstream
-# CS4332-Major-Project
-=======
-# Dynaline
-
-Developed with Unreal Engine 5
->>>>>>> Stashed changes
--- Source/Dynaline/Dynaline.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Dynaline.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dynaline, "Dynaline" );
- 
